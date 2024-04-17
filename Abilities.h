d425a7dc90cc870f9ed29c42d70935776ba83c9d
Volcano-MP <149071205+Project-Z1K@users.noreply.github.com>
2024-04-17 12:20:50 -0700

    Abilities remake (TESTING)

--- Abilities.h
+++ Abilities.h
@@ -1,45 +1,58 @@
 #pragma once
 #include "Includes.h"
+
+// I TESTING IT ITS REMAKE PROB ASSSSSSSSSSSSSS
 namespace Abilities {
-	void GrantAbility(AFortPlayerPawn* Pawn, UGameplayAbility* AbilityClass) {
-		//Make the Gameplay Ability Spec
-		FGameplayAbilitySpecHandle Handle{};
-		Handle.Handle = rand();
-		FGameplayAbilitySpec Spec = FGameplayAbilitySpec{ -1, -1, -1, Handle, (UGameplayAbility*)AbilityClass, 1, -1, nullptr, 0, false, false, false };;
-		static auto GiveAbility = reinterpret_cast<FGameplayAbilitySpecHandle(*)(UAbilitySystemComponent*, const FGameplayAbilitySpecHandle*, FGameplayAbilitySpec)>(uintptr_t(GetModuleHandle(0)) + Offsets::GiveAbility);
-		GiveAbility(Pawn->AbilitySystemComponent, &Handle, Spec);
-	}
+    void GrantAbility(AFortPlayerPawn* Pawn, UGameplayAbility* AbilityClass) {
+        if (!Pawn || !Pawn->AbilitySystemComponent || !AbilityClass) {
+            return;
+        }
 
-	void GiveBaseAbilities(AFortPlayerPawn* Pawn) {
-		static auto Jump = UObject::FindObjectFast<UGameplayAbility>("Default__FortGameplayAbility_Jump");
-		static auto Sprint = UObject::FindObjectFast<UGameplayAbility>("Default__FortGameplayAbility_Sprint");
-		static auto Consumable = UObject::FindObjectFast<UGameplayAbility>("Default__GA_DefaultPlayer_Consumable_C");
-		static auto Shoot = UObject::FindObjectFast<UGameplayAbility>("Default__GA_Ranged_GenericDamage_C");
-		static auto Use = UObject::FindObjectFast<UGameplayAbility>("Default__GA_DefaultPlayer_InteractUse_C");
-		static auto Search = UObject::FindObjectFast<UGameplayAbility>("Default__GA_DefaultPlayer_InteractSearch_C");
-		
-		GrantAbility(Pawn, Jump);
-		GrantAbility(Pawn, Sprint);
-		GrantAbility(Pawn, Consumable);
-		GrantAbility(Pawn, Shoot);
-		GrantAbility(Pawn, Use);
-		GrantAbility(Pawn, Search);
-	}
+        // Crear la especificación de la capacidad de juego
+        FGameplayAbilitySpecHandle Handle{};
+       // Handle.Handle = FMath::Rand();
+        FGameplayAbilitySpec Spec = FGameplayAbilitySpec{
+            -1, -1, -1, Handle, AbilityClass, 1, -1, nullptr, 0, false, false, false
+        };
 
-	FGameplayAbilitySpec* FindAbilitySpecFromHandle(UAbilitySystemComponent* AbilitySystemComponent, FGameplayAbilitySpecHandle Handle)
-	{
-		auto Specs = AbilitySystemComponent->ActivatableAbilities.Items;
+        if (Pawn->AbilitySystemComponent) {
+           //Pawn->AbilitySystemComponent->GiveAbility(FGameplayAbilitySpec(Spec));
+        }
+    }
 
-		for (int i = 0; i < Specs.Num(); i++)
-		{
-			auto& Spec = Specs[i];
+    void GiveBaseAbilities(AFortPlayerPawn* Pawn) {
+        if (!Pawn) {
+            return;
+        }
 
-			if (Spec.Handle.Handle == Handle.Handle)
-			{
-				return &Spec;
-			}
-		}
+        static UGameplayAbility* Jump = UObject::FindObject<UGameplayAbility>("Default__FortGameplayAbility_Jump");
+        static UGameplayAbility* Sprint = UObject::FindObject<UGameplayAbility>("Default__FortGameplayAbility_Sprint");
+        static UGameplayAbility* Consumable = UObject::FindObject<UGameplayAbility>("Default__GA_DefaultPlayer_Consumable_C");
+        static UGameplayAbility* Shoot = UObject::FindObject<UGameplayAbility>("Default__GA_Ranged_GenericDamage_C");
+        static UGameplayAbility* Use = UObject::FindObject<UGameplayAbility>("Default__GA_DefaultPlayer_InteractUse_C");
+        static UGameplayAbility* Search = UObject::FindObject<UGameplayAbility>("Default__GA_DefaultPlayer_InteractSearch_C");
 
-		return nullptr;
-	}
+        GrantAbility(Pawn, Jump);
+        GrantAbility(Pawn, Sprint);
+        GrantAbility(Pawn, Consumable);
+        GrantAbility(Pawn, Shoot);
+        GrantAbility(Pawn, Use);
+        GrantAbility(Pawn, Search);
+    }
+
+    FGameplayAbilitySpec* FindAbilitySpecFromHandle(UAbilitySystemComponent* AbilitySystemComponent, FGameplayAbilitySpecHandle Handle) {
+        if (!AbilitySystemComponent) {
+            return nullptr;
+        }
+
+        TArray<FGameplayAbilitySpec>& Specs = AbilitySystemComponent->ActivatableAbilities.Items;
+
+        for (int i = 0; i < Specs.Num(); i++) {
+            if (Specs[i].Handle.Handle == Handle.Handle) {
+                return &Specs[i];
+            }
+        }
+
+        return nullptr;
+    }
 }
